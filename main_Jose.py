18ec9314ffcbe11fb84385e35d8d802b7a33d3bb
Jose-M-O <111915879+Jose-M-O@users.noreply.github.com>
2023-12-03 21:17:37 -0500

    Updated main_Jose.py

--- main_Jose.py
+++ main_Jose.py
@@ -1,11 +1,10 @@
+import matplotlib.pyplot as plt
 from RaceThreatLevel import RaceThreatLevelStats
 import police_shootings
 shootings = police_shootings.get_shootings()
 
 extracted_threat = [person['Factors']['Threat-Level'] for person in shootings if 'Factors' in person and 'Threat-Level' in person['Factors']]           #Extract threat from factors
 extracted_race = [person['Person']['Race'] for person in shootings if 'Person' in person and 'Race' in person['Person']]            #Extract race from person
-
-
 
 
 
@@ -19,9 +18,15 @@ if __name__ == '__main__':
 
 
     print(race_counts)
-    print(threat_level_counts)
+    print(threat_level_counts)            #Prints all data
     print(race_threat_rates)
-
+    lists = sorted(race_threat_rates.items())
+    x, y = zip(*lists)
+    plt.figure(figsize=(12, 5))
+    plt.plot(x, y)
+    plt.xlabel("Race")            #Plots data to line graph with proper spacing and labeling
+    plt.ylabel("\"Threat\" Rate")
+    plt.show()
 
 
 
