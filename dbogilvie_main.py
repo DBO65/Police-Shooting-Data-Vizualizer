dcd8d6e5d489b850c6ebf864ec8a3f0838d1fd8f
DBO65 <73730800+DBO65@users.noreply.github.com>
2023-12-06 12:54:43 -0500

    Update dbogilvie_main.py

--- dbogilvie_main.py
+++ dbogilvie_main.py
@@ -1,8 +1,8 @@
+import police_shootings
 from armedfleeingstats import ArmedFleeingStats
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import police_shootings
 shootings = police_shootings.get_shootings()
 
 extract_person = [person for person in shootings]
@@ -21,6 +21,7 @@ AF_stats.b_armed_fleeing_count()    # The b in b_armed_fleeing_count() stands fo
 AF_stats.a_armed_fleeing_count()    # The a in a_armed_fleeing_count() stands for asian
 AF_stats.h_armed_fleeing_count()    # The h in h_armed_fleeing_count() stands for hispanic
 AF_stats.na_armed_fleeing_count()   # The na in na_armed_fleeing_count() stands for native american
+AF_stats.u_armed_fleeing_count()   # The u in u_armed_fleeing_count() stands for unknown
 
 c_f_data = len(extract_fleeing)/len(extract_person)         # c_f_data stands for Control Fleeing Data
 c_af_data = len(AF_stats.a_flee)/len(extract_fleeing)       # af in c_af_data stands for Armed Fleeing
@@ -29,6 +30,7 @@ b_af_data = len(AF_stats.b_a_fleeing)/len(AF_stats.black)   # b in b_af_data sta
 a_af_data = len(AF_stats.a_a_fleeing)/len(AF_stats.asian)   # a in a_af_data stands for Asian
 h_af_data = len(AF_stats.h_a_fleeing)/len(AF_stats.hispanic)    # h in h_af_data stands for Hispanic
 na_af_data = len(AF_stats.na_a_fleeing)/len(AF_stats.native)    # na in na_af_data stands for Native American
+u_af_data = len(AF_stats.u_a_fleeing)/len(AF_stats.unknown)     # u in u_af_data stands for Unknown
 
 waf_data = (len(AF_stats.w_a_fleeing)/len(extract_fleeing))     # waf_data stands for white armed fleeing data
 baf_data = len(AF_stats.b_a_fleeing)/len(extract_fleeing)       # baf_data stands for black armed fleeing data
@@ -36,12 +38,14 @@ aaf_data = len(AF_stats.a_a_fleeing)/len(extract_fleeing)       # aaf_data stand
 haf_data = len(AF_stats.h_a_fleeing)/len(extract_fleeing)       # haf_data stands for hispanic armed fleeing data
 _na_af_data = len(AF_stats.na_a_fleeing)/len(extract_fleeing)
 # _na_af_data stands for native american armed fleeing data
+uaf_data = len(AF_stats.u_a_fleeing)/len(extract_fleeing)       # uaf_data stands for unknown armed fleeing data
 
 c_waf_data = len(AF_stats.w_a_fleeing)/len(extract_person)   # c_waf_data stands for control white armed fleeing data
 c_baf_data = len(AF_stats.b_a_fleeing)/len(extract_person)   # Similar logic to c_waf_data
 c_aaf_data = len(AF_stats.a_a_fleeing)/len(extract_person)   # Similar logic to c_waf_data
 c_haf_data = len(AF_stats.h_a_fleeing)/len(extract_person)   # Similar logic to c_waf_data
 c_naf_data = len(AF_stats.na_a_fleeing)/len(extract_person)  # Similar logic to c_waf_data
+c_uaf_data = len(AF_stats.u_a_fleeing)/len(extract_person)   # Similar logic to c_waf_data
 
 print(f"\nFor police shootings in 2015 the {c_f_data*100:.2f}% of those involved in fatal police shootings were "
       f"fleeing")
@@ -57,41 +61,45 @@ print(f"Of all the Hispanic individuals involved in a fatal police shooting {h_a
       f"while armed")
 print(f"Of all the Native American individuals involved in a fatal police shooting {na_af_data*100:.2f}% were fleeing "
       f"while armed")
+print(f"Of all the Unknown individuals involved in a fatal police shooting {u_af_data*100:.2f}% were fleeing "
+      f"while armed")
 
 print(f"\nOut of the population of individuals fleeing a fatal police shooting while armed {waf_data*100:.2f}%\n"
       f"were White, {baf_data*100:.2f}% were African American, {aaf_data*100:.2f}% were Asian,\n"
-      f"{haf_data*100:.2f}% were Hispanic and {_na_af_data*100:.2f} where Native American%")
+      f"{haf_data*100:.2f}% were Hispanic, {_na_af_data*100:.2f}% where Native American, "
+      f"and {uaf_data*100:.2f}% were Unknown,")
 
 print(f"\nIn contrast out of all the fatal police shootings that occurred in 2015, {c_waf_data*100:.2f}%\n"
       f"were White, {c_baf_data*100:.2f}% were African American, {c_aaf_data*100:.2f}% were Asian,\n"
-      f"{c_haf_data*100:.2f}% were Hispanic and {c_naf_data*100:.2f}% where Native American%")
+      f"{c_haf_data*100:.2f}% were Hispanic, {c_naf_data*100:.2f}% were Native American,"
+      f"and {c_uaf_data*100:.2f}% were Unknown")
 
 # Create and Display bar chart
-x = ['White', 'African-American', 'Asian', 'Hispanic', 'Native American']
-y = [w_af_data*100, b_af_data*100, a_af_data*100, h_af_data*100, na_af_data*100]
+x = ['White', 'African-American', 'Asian', 'Hispanic', 'Native American', 'Unknown']
+y = [w_af_data*100, b_af_data*100, a_af_data*100, h_af_data*100, na_af_data*100, u_af_data*100]
 plt.bar(x, y, width=1, edgecolor='white', linewidth=0.7)
 plt.title('Bar Graph Showing Percentage of each Race (Excluding Other & Unknown) Involved in Fatal Police Shooting'
-          '\nWhile Fleeing While Armed (2015)')
+          '\nWhile Fleeing And Armed')
 plt.xlabel('\nRaces')
 plt.ylabel('Percentage (%)')
 plt.show()
 
 # Create and Display bar chart
-x = ['White', 'African-American', 'Asian', 'Hispanic', 'Native American']
-y = [waf_data*100, baf_data*100, aaf_data*100, haf_data*100, _na_af_data*100]
+x = ['White', 'African-American', 'Asian', 'Hispanic', 'Native American', 'Unknown']
+y = [waf_data*100, baf_data*100, aaf_data*100, haf_data*100, _na_af_data*100, uaf_data*100]
 plt.bar(x, y, width=1, edgecolor='white', linewidth=0.7)
-plt.title('Bar Graph Showing the Percentage of Individuals Fleeing While Armed in Fatal Police Shootings (2015)'
+plt.title('Bar Graph Showing the Percentage of Individuals Fleeing While Armed in Fatal Police Shootings'
           '\nDistribution Across Different Races')
 plt.xlabel('\nRaces')
 plt.ylabel('Percentage (%)')
 plt.show()
 
 # Create and Display bar chart
-x = ['White', 'African-American', 'Asian', 'Hispanic', 'Native American']
-y = [c_waf_data*100, c_baf_data*100, c_aaf_data*100, c_haf_data*100, c_naf_data*100]
+x = ['White', 'African-American', 'Asian', 'Hispanic', 'Native American', 'Unknown']
+y = [c_waf_data*100, c_baf_data*100, c_aaf_data*100, c_haf_data*100, c_naf_data*100, c_uaf_data*100]
 plt.bar(x, y, width=1, edgecolor='white', linewidth=0.7)
 plt.title('Bar Graph Showing the Percentage of Individuals Fleeing While Armed for Each Race'
-          '\nAs a Proportion of Total Fatal Police Shooting Incidents (2015)')
+          '\nAs a Proportion of Total Fatal Police Shooting Incidents')
 plt.xlabel('\nRaces')
 plt.ylabel('Percentage (%)')
 plt.show()
